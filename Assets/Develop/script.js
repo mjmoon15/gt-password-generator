e65cee6058fa14f26b9bd15392aabfdf79504d0c
Mark Mooney <mjmoon15@gmail.com>
2020-06-07 11:54:44 -0400

    built setLength and conditions functions

--- Assets/Develop/script.js
+++ Assets/Develop/script.js
@@ -14,11 +14,13 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-//password arrays
-let lowerArray=["abcdefghijklmnopqrstuvwxyz"];
-let upperArray=["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-let numberArray=["0123456789"];
-let symbolArray=["~!@#$%^&*?/"];
+//Global variables
+
+var lowerArray=["abcdefghijklmnopqrstuvwxyz".split()];
+var upperArray=["ABCDEFGHIJKLMNOPQRSTUVWXYZ".split()];
+var numberArray=[0,1,2,3,4,5,6,7,8,9];
+var symbolArray=["~!@#$%^&*?/".split()];
+var passwordArray= []
 
 
 //Password generator!!
@@ -32,23 +34,45 @@ let symbolArray=["~!@#$%^&*?/"];
 
   function setLength(){
     var passwordLength = prompt("How long should your password be? Please enter a number between 8-128.")
-      if (passwordLength > 7 || passwordLength < 129){
-        confirm("Would you like to use lower case letters?") 
-      } else prompt("Please enter a number between 8-128")
+      if (passwordLength >= 8 || passwordLength <= 128){
+        console.log("Your password will be " + passwordLength + " characters long."); 
+      } else if (passwordLength = NaN){
+        prompt("Please enter a number between 8-128")
+      } else {
+        prompt("Please enter a number between 8-128")
+      } 
   }
+  
   setLength()
 
-//If length meets criteria b/w 8-128 characters, prompt user to select character types
-//lowercase
-  function chooseLower(){
-    
-  
+//Conditional variables
 
-}
-//Choose at least 1 character type
+var lowers = confirm("Would you like to use lower case letters?");
+var uppers = confirm("Would you like to use upper case letters?");
+var numbers = confirm("Would you like to use numbers?");
+var symbols = confirm("Would you like to use symbols?");
+
+//If length meets criteria b/w 8-128 characters, prompt user to select character types
+  function conditions(){
+    
+    if (lowers){
+      passwordArray=lowerArray.push
+    } else if (uppers){
+      passwordArray=upperArray.push
+    } else if (numbers){
+      passwordArray=numberArray.push
+    } else if (symbols){
+      passwordArray=symbolArray.push
+    }
+  }
+
+  conditions()
+  console.log(passwordArray)
+
 
 //password array
-function buildPassword([]){
+function buildPassword(){
+
 
 }
 
